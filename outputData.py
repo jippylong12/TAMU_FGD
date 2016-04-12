760aee6e1f0a3cd1bafbf15ec3410af35774fdad
Marcus <markysalinas12@gmail.com>
2016-04-12 01:03:54 -0500

    The program is done. Not tested at all for other other colleges besides Engieering and Agriculture but I think it will work. If it does work then all that is left is running it on all the colleges and then eventually if I wanted to somehow combining all the data. from the past 3 years or something.

--- outputData.py
+++ outputData.py
@@ -20,11 +20,24 @@ def outputData(masterDictionary,title):
 
     #'{:.1%}'.format(1/3.0)
 
+    #writing class data to spreadsheet if not using sortByCourse Method
+    for courseName,dataList in masterDictionary.items():
+        ws.cell(row = rowCount, column = 1).value = courseName
+        rowCount += 1
+        for item in dataList:
+            professorName = item.keys()[0]
+            ws.cell(row = rowCount, column = 2).value = professorName
+            ws.cell(row = rowCount, column = 3).value = item[professorName][13]
+            ws.cell(row = rowCount, column = 4).value = '{:.2%}'.format(item[professorName][8])
+            ws.cell(row = rowCount, column = 5).value = '{:.2%}'.format(item[professorName][9])
+            ws.cell(row = rowCount, column = 6).value = '{:.2%}'.format(item[professorName][10])
+            ws.cell(row = rowCount, column = 7).value = '{:.2%}'.format(item[professorName][11])
+            ws.cell(row = rowCount, column = 8).value = '{:.2%}'.format(item[professorName][12])
+            rowCount += 1
+        rowCount+=1
 
-        # writing class data to spreadsheet
 
-
-
+    print "Here"
 
 
 
