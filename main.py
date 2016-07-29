f1a56d29fd43af7f8aa5270754de942e6f642640
Marcus <markysalinas12@gmail.com>
2016-07-29 12:34:45 -0500

    uncommited the main

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ listOfColleges = [
     "AG", # AGRICULTURE AND LIFE SCIENCES
     "AR", # ARCHITECTURE
     "BA", # BUSINESS ADMINISTRATION
-    #"ED", # EDUCATION Spring 2016
+    "ED", # EDUCATION Spring 2016
     "EL", # ENGLISH LANGUAGE INSTITUTE
     "EN", # ENGINEERING
     "GB", # GEORGE BUSH SCHOOL OF GOVERNMENT
     "GE", # GEOSCIENCES
-    #"LA", # LIBERAL ARTS Spring 2014
-    #"MD", # MEDICINE No longer have access
+    "LA", # LIBERAL ARTS Spring 2014
+    # "MD", # MEDICINE No longer have access
     "MS", # MILITARY SCIENCE
     "SC", # SCIENCE
     "VM"  # VETERINARY MEDICINE
@@ -50,49 +50,49 @@ listOfSemesters = [
     #"Fall"  # C
 ]
 
-year = 2016
+year = 2014
 MainDirectory = os.getcwd()
-# for semester in listOfSemesters:
-#     print ("On Semester: " + semester)
-#     os.chdir(MainDirectory)
-#     semesterChar = getSemesterChar(semester)
-#     folderName = semester + str(year)
-#     pdfFileDirectory = os.getcwd() + "\\GradeDistributionsDB\\" + folderName
-#     # # Part 1a
-#     # # get the data from the website
-#     for x in xrange(0,len(listOfColleges)):
-#         print("On College: " + str(listOfColleges[x]))
-#         downloadPDFs(url,str(year),semesterChar,listOfColleges[x])
-#
-#     os.chdir(pdfFileDirectory)
-#     # # Part 1b
-#     # # take the pdfs and make them to text files
-#     pdfList = glob('*.pdf')
-#     googleOCR(folderName,pdfList)
-#
-#     # Part 2a
-#     # take all the data we have right now and give us what we need
-#     txtList = glob('*.txt')
-#     for textFile in txtList:
-#         os.chdir(MainDirectory)
-#         print ("On TextFile " + textFile)
-#         college = textFile[8:10]
-#         masterDictionary = manipulatePdfs(textFile,semester,str(year))
-#
-#         # Part 2b
-#         # take the data we have and make it useful
-#         title = semester+str(year)+ " " + college + ".xlsx"
-#         wb = Workbook()
-#
-#         #save the file to a new path
-#         newPath = os.getcwd() + "\\Output"
-#         if not os.path.exists(newPath):
-#             os.makedirs(newPath)
-#         os.chdir(newPath)
-#
-#         # call the function to outpt data and save in the new path
-#         wb = outputData(masterDictionary,title)
-#         wb.save(title)
+for semester in listOfSemesters:
+    print ("On Semester: " + semester)
+    os.chdir(MainDirectory)
+    semesterChar = getSemesterChar(semester)
+    folderName = semester + str(year)
+    pdfFileDirectory = os.getcwd() + "\\GradeDistributionsDB\\" + folderName
+    # # Part 1a
+    # # get the data from the website
+    for x in xrange(0,len(listOfColleges)):
+        print("On College: " + str(listOfColleges[x]))
+        downloadPDFs(url,str(year),semesterChar,listOfColleges[x])
+
+    os.chdir(pdfFileDirectory)
+    # # Part 1b
+    # # take the pdfs and make them to text files
+    pdfList = glob('*.pdf')
+    googleOCR(folderName,pdfList)
+
+    # Part 2a
+    # take all the data we have right now and give us what we need
+    txtList = glob('*.txt')
+    for textFile in txtList:
+        os.chdir(MainDirectory)
+        print ("On TextFile " + textFile)
+        college = textFile[8:10]
+        masterDictionary = manipulatePdfs(textFile,semester,str(year))
+
+        # Part 2b
+        # take the data we have and make it useful
+        title = semester+str(year)+ " " + college + ".xlsx"
+        wb = Workbook()
+
+        #save the file to a new path
+        newPath = os.getcwd() + "\\Output"
+        if not os.path.exists(newPath):
+            os.makedirs(newPath)
+        os.chdir(newPath)
+
+        # call the function to outpt data and save in the new path
+        wb = outputData(masterDictionary,title)
+        wb.save(title)
 
 # finally we just run the createMaster DB file
 os.chdir(MainDirectory)
